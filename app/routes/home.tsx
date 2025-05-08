738dc5c319a32e8e40a8fe4276340c9c9a0796eb
leralys <valerialysko@gmail.com>
2025-05-08 14:09:03 +0300

    chore: cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Cross Stitch Generator" },
+    { name: "description", content: "A cross stitch pattern generator app" },
   ];
 }
 
