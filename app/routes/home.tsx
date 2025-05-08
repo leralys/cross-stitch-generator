5e42ebc336e1e00a0af0375433ccf1d4db0205d0
leralys <valerialysko@gmail.com>
2025-05-08 14:35:18 +0300

    chore: prettier

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Welcome } from '../welcome/welcome';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Cross Stitch Generator" },
-    { name: "description", content: "A cross stitch pattern generator app" },
+    { title: 'Cross Stitch Generator' },
+    { name: 'description', content: 'A cross stitch pattern generator app' },
   ];
 }
 
